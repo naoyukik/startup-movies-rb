d0541e072cac7a08db62f8e5428b5e4f1be2b4a8
naoyuki kodama <mmaga@dstribe.net>
2015-05-04 18:23:51 +0900

    Issue #5 · フロントエンド作成 controllersにSIteDtaの件数を取得する関数を追加,　ソースの整理

--- controllers.rb
+++ controllers.rb
@@ -1,13 +1,11 @@
 # coding: utf-8
 
-require 'json'
-require 'date'
-
 #
 # サイトindex
 # 登録フォーム、ページングくらいの機能しか実装されない
 #
 get '/index' do
+  @max_item_count = SiteData.count
   erb :index
 end
 
@@ -41,12 +39,22 @@ get '/api/site' do
   tag_result = []
   page_num = 1
   # 登録site取得
-  if defined? params[:page]
-    page_num = params[:page]
+  if defined?(params[:page]) && params[:page].to_i > 0
+    page_num = params[:page].to_i
   end
-  SiteData.page(page_num).each do |site_data|
+  SiteData.paginate(:page => page_num, :per_page => 1).each do |site_data|
     object = load_provider(site_data['provider'])
     tag_result << object.create_embed_data(site_data)
   end
+  tag_result.to_json
+
+end
+
+#
+# [API]SiteDataの件数取得
+#
+get '/api/count_data' do
+  tag_result = []
+  tag_result << SiteData.count
   tag_result.to_json
 end
